73287a6f419c9b1e3dcaabe2f1a6186f5a9d9f14
mayankagarwal44442 <mayankagarwal44442@gmail.com>
2020-04-26 18:27:57 +0530

    made list indexable

--- generic_skip_list.cpp
+++ generic_skip_list.cpp
@@ -2,13 +2,7 @@
 
 #include <iostream>
 #include <unordered_map>
-
-#define MAX_OF(type) \
-	(((type)(~0LLU) > (type)((1LLU << ((sizeof(type) << 3) - 1)) - 1LLU)) ? (long long unsigned int)(type)(~0LLU) : (long long unsigned int)(type)((1LLU << ((sizeof(type) << 3) - 1)) - 1LLU))
-
-//(type)((1LLU<<((sizeof(type)<<3)-1))-1LLU)) retrieves the largest possible value using n-1 bits if n bits is total size
-//~0LLU would retreive largest value using all bits.
-//The condition checks whether type is unsigned or signed and gives appropriate maximum value
+#include <vector>
 
 using namespace std;
 
@@ -40,6 +34,7 @@ private:
 			delete[] next;
 			delete[] widths;
 		}
+	
 	};
 
 public:
@@ -47,12 +42,12 @@ public:
 	{
 		level = 0; //we start off with just one level
 
-		head = new Node(MAX_OF(T), max_level_);
+		head = new Node(0, max_level_);				//value of head node doesn't matter.
 
 		for (int i = 0; i <= max_level_; ++i)
 		{
 			head->next[i] = head; //initially all levels are pointing to itself (empty skip list)
-			head->widths[i] = 0;
+			head->widths[i] = 1;
 		}
 
 		head->prev = head; //level 0 back link.
@@ -92,16 +87,18 @@ public:
 
 		for (int i = level; i > 0; --i) //correct all head links
 		{
-			head->next[i] = ht[rhs.head->next[i]];
+			head -> next[i] = ht[rhs.head -> next[i]];
+			head -> widths[i] = rhs.head -> widths[i];
 		}
 		head->prev = ht[rhs.head->prev];
 		p = rhs.head->next[0];
 		p_clone = head->next[0];
 		while (p != rhs.head) //do the same for all rest nodes
 		{
-			for (int i = p->level; i > 0; --i) //correct all head links
+			for (int i = p->level; i > 0; --i) //correct all  links
 			{
-				p_clone->next[i] = ht[p->next[i]];
+				p_clone -> next[i] = ht[p -> next[i]];
+				p_clone -> widths[i] = p -> widths[i];
 			}
 			p_clone->prev = ht[p->prev];
 			p = p->next[0];
@@ -142,7 +139,9 @@ public:
 
 			for (int i = level; i > 0; --i) //correct all head links
 			{
-				head->next[i] = ht[rhs.head->next[i]];
+				head -> next[i] = ht[rhs.head->next[i]];
+				head -> widths[i] = rhs.head -> widths[i];
+
 			}
 			head->prev = ht[rhs.head->prev];
 			p = rhs.head->next[0];
@@ -152,6 +151,8 @@ public:
 				for (int i = p->level; i > 0; --i) //correct all head links
 				{
 					p_clone->next[i] = ht[p->next[i]];
+					p_clone -> widths[i] = p -> widths[i];
+
 				}
 				p_clone->prev = ht[p->prev];
 				p = p->next[0];
@@ -160,6 +161,7 @@ public:
 		}
 		return *this;
 	}
+
 
 	// bidirectional iterator
 	class iterator
@@ -218,6 +220,21 @@ public:
 		return iterator(head);
 	}
 
+	iterator operator[](int index)
+	{
+		Node* p = head;
+		index += 1;
+		for (int i = level; i >= 0; --i)
+		{
+			while (p -> widths[i] <=  index)
+			{
+				index -= p -> widths[i];
+				p = p -> next[i];
+			}
+		}
+		return iterator(p);
+	}
+
 	void insert_node(T key);
 	void delete_node(T key);
 	typename SkipList<T>::iterator search(T key);
@@ -228,7 +245,7 @@ private:
 	int rand_level()
 	{
 		int level = 0;
-		while (rand() < RAND_MAX / 2 && level < max_level_) //1/2 probability to increase level., we keep level < max_level_ as condition so that even if level overflows it is a valid level.
+		while (rand() > RAND_MAX / 4 && level < max_level_) //1/2 probability to increase level., we keep level < max_level_ as condition so that even if level overflows it is a valid level.
 			++level;
 		return level;
 	}
@@ -244,12 +261,17 @@ void SkipList<T>::insert_node(T key)
 	Node *newnode_prev[max_level_ + 1]; //array of pointers to nodes. These are those nodes which will point to the  node being inserted.
 	Node *p = head;
 
+	vector<int> steps(level+1);
 	for (int i = level; i >= 0; --i)
 	{
-		while (p->next[i]->val < key)
+		while ((p -> next[i] -> val < key) && (p -> next[i] != head))
+		{
 			p = p->next[i];
+			steps[i] += 1;
+		}
 		newnode_prev[i] = p;
 	}
+
 	p = p->next[0]; //now p is the node with val either past the value or equal to value
 
 	if (key == p->val) //duplicate
@@ -263,18 +285,66 @@ void SkipList<T>::insert_node(T key)
 		level = newnode_level;
 	}
 
-	p = new Node(key, newnode_level); //p is the node to be inserted
+	vector<int> widths(newnode_level+1, 1);
+	for(int i = 1; i <= newnode_level; ++i)
+	{
+		widths[i] = steps[i-1] + widths[i-1];
+	}
 
-	// TODO: add increment-decrement logic for widths
+
+
+	p = new Node(key, newnode_level); //p is the node to be inserted
+	p -> widths[0] = 1;				//base level widths is 1. 
+	
+
 
 	newnode_prev[0]->next[0]->prev = p; //stiching back link.
 	p->prev = newnode_prev[0];			//back link for created node
+
+
+	cout << "Widths before : ";
+	for(int i = 0; i <= max_level_; ++i)
+	{	
+		if(i <= newnode_level)
+			cout << newnode_prev[i]->widths[i] << ' ';
+		else
+			cout << head -> widths[i] << ' ';
+	}
+		
+	cout << '\n';
 
 	for (int i = 0; i <= newnode_level; ++i) //stitch new node between
 	{
 		p->next[i] = newnode_prev[i]->next[i];
 		newnode_prev[i]->next[i] = p;
+		p -> widths[i] = newnode_prev[i] -> widths[i] - widths[i] + 1;
+		newnode_prev[i] -> widths[i] = widths[i];
 	}
+
+	if(newnode_level < level)
+	{
+		for(int i = newnode_level + 1; i <= level; ++i)
+			newnode_prev[i] -> widths[i] += 1;
+	}
+
+	for(int i = level + 1; i <= max_level_; ++i)
+		head -> widths[i] += 1;
+
+	cout << "Widths of prev After : ";
+	for(int i = 0; i <= max_level_; ++i)
+	{	
+		if(i <= newnode_level)
+			cout << newnode_prev[i]->widths[i] << ' ';
+		else
+			cout << head -> widths[i] << ' ';
+	}
+	cout << '\n';
+	cout << "Widths of inserted node : ";
+	for(int i = 0; i <= newnode_level; ++i)
+		cout << p->widths[i] << ' ';
+	cout << '\n';
+
+
 }
 
 template <typename T>
@@ -285,7 +355,7 @@ void SkipList<T>::delete_node(T key)
 
 	for (int i = level; i >= 0; --i)
 	{
-		while (p->next[i]->val < key)
+		while ((p -> next[i] -> val < key) && (p -> next[i] != head))
 			p = p->next[i];
 		newnode_prev[i] = p;
 	}
@@ -299,6 +369,7 @@ void SkipList<T>::delete_node(T key)
 			if (newnode_prev[i]->next[i] != p)
 				break;
 			newnode_prev[i]->next[i] = p->next[i];
+			newnode_prev[i] -> widths[i] += p -> widths[i] - 1;	//adjust widths
 		}
 
 		delete p;
@@ -315,7 +386,7 @@ typename SkipList<T>::iterator SkipList<T>::search(T key)
 
 	for (int i = level; i >= 0; --i)
 	{
-		while (p->next[i]->val < key)
+		while ((p -> next[i] -> val < key) && (p -> next[i] != head))
 			p = p->next[i];
 	}
 	if (p->next[0]->val == key)
@@ -350,13 +421,17 @@ void SkipList<T>::back_link_test()
 int main()
 {
 	srand(3);
-	SkipList<int> list(3);
-	int arr[] = {3, 7, 5, 2};
+	SkipList<int> list(6);
+	int arr[] = {3, 7, 5, 2, 9, 11, 4};
 	for (int i = 0; i < sizeof(arr) / sizeof(int); ++i)
 		list.insert_node(arr[i]);
 
 	list.print();
 	//list.back_link_test();
+
+	SkipList<int>::iterator res = list[4];
+	cout << *res << '\n';
+	
 
 #if 0
 //copy constructor test
@@ -379,7 +454,7 @@ int main()
 	// list.back_link_test();
 #endif
 
-#if 1
+#if 0
 	SkipList<int>::iterator res = list.search(2);
 	if (res == list.end())
 		cout << "Not found!\n";
